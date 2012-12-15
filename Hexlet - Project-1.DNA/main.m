3a45b212fcb86a373f35274698a40273452360d3
Amka Me <meamka@ya.ru>
2012-12-15 16:50:50 +0400

    Initial Commit

---
+++ Hexlet - Project-1.DNA/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  Hexlet - Project-1.DNA
+//
+//  Created by Amka Me on 15.12.12.
+//  Copyright (c) 2012 Amka Me. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
